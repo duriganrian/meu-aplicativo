ccd5c3da102aa6154ebe58e8fc74e5e6c13ddd70
Rian Durigan <durigansrian@gmail.com>
2024-05-24 16:01:47 -0300

    Arrumando o contato

--- src/Contato.jsx
+++ src/Contato.jsx
@@ -5,12 +5,16 @@ import { useNavigate, useLocation } from 'react-router-dom';
 function Contato() {
   const navigate = useNavigate();
   const location = useLocation();
-  const objeto = location.state.objeto;
+  const objeto = location.state && location.state.objeto;
 
   return (
     <div className="contato-container">
       <h1 className="contato-title">Página de Contato</h1>
-      <p>Meu nome é {objeto}</p>
+      {objeto ? (
+        <p>Meu nome é {objeto}</p>
+      ) : (
+        <p>Meu nome é Rian Durigan</p>
+      )}
     </div>
   );
 };
